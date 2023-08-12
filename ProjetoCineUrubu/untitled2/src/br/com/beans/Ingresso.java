50df0b888995355ff1a47aa10139568719e49971
Daniel Tavares de Lima Marcelino <78553124+Daniel-Tavares-de-Lima@users.noreply.github.com>
2023-08-12 20:47:55 -0300

    Update Ingresso.java
    Adicionei os métodos getters e setters e adicionei mais duas variáveis a classe.

--- ProjetoCineUrubu/untitled2/src/br/com/beans/Ingresso.java
+++ ProjetoCineUrubu/untitled2/src/br/com/beans/Ingresso.java
@@ -10,6 +10,9 @@ public class Ingresso {
     private String tipo;
     private Filme filme;
 
+    private int id;
+    private double Preco;
+
     public Ingresso(Usuario cliente, Sessao sessao, LocalDateTime hora, Assento assento, String tipo, Filme filme){
         this.cliente = cliente;
         this.sessao = sessao;
@@ -18,4 +21,39 @@ public class Ingresso {
         this.tipo = tipo;
         this.filme = filme;
     }
+
+    public Usuario getCliente(){
+        return cliente;
+    }
+
+    public Sessao getSessao(){
+        return sessao;
+    }
+
+    public LocalDateTime getHora(){
+        return hora;
+    }
+
+    public Assento getAssento(){
+        return assento;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+
+    public Filme getFilme(){
+        return filme;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public double setPreco(double preco){
+        return Preco;
+    }
+
+    
 }
+
